b18687777811dd8fa7f14c5516261b7c90085d08
Guilherme Oliveira <guioliveira99@gmail.com>
2022-12-12 01:33:09 -0300

    Adding migration to server fields

---
+++ VideoMonitor/Migrations/20221212043215_AddServerFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoMonitor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddServerFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ip",
+                table: "Servers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Servers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Port",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ip",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "Port",
+                table: "Servers");
+        }
+    }
+}
